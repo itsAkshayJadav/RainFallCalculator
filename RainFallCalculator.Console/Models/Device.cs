4b8bddcd4ae5e8d92eba1272b4682a507ca60712
itsAkshayJadav <jdvaks5758@gmail.com>
2026-03-22 10:32:54 +0800

    Add domain models

---
+++ RainFallCalculator.Console/Models/Device.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RainfallCalculator.Console.Models;
+
+public class Device
+{
+    public string DeviceId { get; set; } = string.Empty;
+    public string DeviceName { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+}
---
+++ RainFallCalculator.Console/Models/DeviceSummary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RainfallCalculator.Console.Models;
+
+public class DeviceSummary
+{
+    public string DeviceId { get; set; } = string.Empty;
+    public string DeviceName { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public decimal AverageRainfallLast4Hours { get; set; }
+    public string Status { get; set; } = string.Empty;
+    public string Trend { get; set; } = string.Empty;
+}
